cdaf49419ce533fef25a60914aea2769bde7402f
Mohcine Otmane <littlesandflea@gmail.com>
2025-04-24 23:39:03 +0100

    More for loop examples

--- number_sequence.sh
+++ number_sequence.sh
@@ -1,5 +1,29 @@
 read n 
 
+# Print numbers from 1 to n
 for i in $( seq 1 "$n"); do
     echo "$i"
 done
+
+# Print squares of numbers from 1 to n
+
+for i in $( seq 1 "$n"); do
+    echo "$((i*i))"
+done
+
+# Print even numbers from 1 to n
+for i in $( seq 2 "$n" 2); do
+    echo "$i"
+done
+
+
+# Print the sum of numbers from 1 to n
+read n
+s=0
+for i in $(seq 1 "$n"); do
+    s=$((s + i))
+done
+echo "The sum of numbers from 1 to $n is $s"
+
+
+
