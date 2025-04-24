e7ebd47af187b19ad1a24882db73b81fde93d230
Mohcine Otmane <littlesandflea@gmail.com>
2025-04-25 00:14:57 +0100

    Functions

---
+++ functions.sh
@@ -0,0 +1,12 @@
+# $1 first argument $2 second argument
+add() {
+    echo $(($1+$2))
+}
+add 1 5
+
+# Read argument
+sayhello() {
+    echo "Hello $1"
+}
+read -p "Enter your name: " name
+sayhello "$name"
