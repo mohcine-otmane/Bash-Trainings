656f78ba2f04b995425030b37a0f4771fe7464f5
Mohcine Otmane <littlesandflea@gmail.com>
2025-04-26 23:52:02 +0100

    File hadling

--- studentscores.sh
+++ studentscores.sh
@@ -11,3 +11,20 @@ echo "Name: $name"
 echo "Age: $age"
 
 echo "Scores: ${score[@]}"
+
+file = $1.json
+if[-e $policy ]; then
+    echo "File exists"
+else
+    cat > $file <<EOF
+    {
+        "name": "$name",
+        "age": $age,
+        "scores": [
+            ${score[1]},
+            ${score[2]},
+            ${score[3]}
+        ]
+    }
+    EOF
+fi
